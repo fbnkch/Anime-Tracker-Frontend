b9b29f0388c88d0911d2c66f228ea4ea22b3ba13
Fabian Koch <F.Koch@Student.HTW-Berlin.de>
2025-04-30 16:33:41 +0200

    Initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue'
+import Options from 'vue-class-component'
+
+@Options({})
+export default class NavBar extends Vue {
+
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
