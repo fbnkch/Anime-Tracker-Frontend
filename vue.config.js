6d023ac60d9f2c906562bf29d0fe324b8b301d19
Fabian Koch <F.Koch@Student.HTW-Berlin.de>
2025-05-01 15:54:51 +0200

    updated vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { defineConfig } = require('@vue/cli-service');
 
-module.exports = {
+module.exports = defineConfig({
+  transpileDependencies: true,
   devServer: {
-    port: '3000'
+    port: 3000
   },
   lintOnSave: false
-}
+});
